30efa7ba047bfa5a25b21aef6278987c5270c243
AJ Villalobos <hi@ajvillalobos.com>
2018-11-11 19:54:10 +0800

    Setup now deployment

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Head from "next/head";
-import "../static/style.css";
 
 const Meta = () => (
   <Head>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import "../styles/index.css";
 import React from "react";
 import App, { Container } from "next/app";
 import Page from "../components/Page";
